04abea341bd1b223f4264da477a8f62eecb14493
Fredstyle93 <fred.beaulieu123@gmail.com>
2019-03-31 11:23:31 -0400

    Git, Firebase & react init

---
+++ src/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBGwuM0IoD9RYfoom2C1R7nCM3X_z4Sqoo",
+    authDomain: "gym-application-1ece2.firebaseapp.com",
+    databaseURL: "https://gym-application-1ece2.firebaseio.com",
+    projectId: "gym-application-1ece2",
+    storageBucket: "gym-application-1ece2.appspot.com",
+    messagingSenderId: "531584747371"
+};
+
+const fire = firebase.initializeApp(config);
+
+export default fire;
